0cdeb4136774a48bdd062cea4157e47652e6c11d
Thiago Oliveira <thiago.dev18@gmail.com>
2022-07-02 15:31:10 -0300

    created pages

---
+++ lib/features/movie/presentation/ui/pages/details_page.dart
@@ -0,0 +1,50 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/utils/apis_utils.dart';
+import 'package:movie_app/features/movie/domain/entities/entities.dart';
+
+class DetailsPage extends StatelessWidget {
+  final MovieDetailsEntity movie;
+  const DetailsPage({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .55,
+                width: MediaQuery.of(context).size.width,
+                child: Hero(
+                  tag: movie.id,
+                  child: CachedNetworkImage(
+                    imageUrl: API.REQUEST_IMG(movie.posterPath),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                movie.overview,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.new_releases),
+                  Text(movie.releaseDate),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
